62c6d6e3054db5b1552b22fb486baacb3573c4a1
alexandrachiriac34 <alexandrachiriacc90@gmail.com>
2026-05-28 19:06:44 +0300

    rezolvarea finala

--- web/app.py
+++ web/app.py
@@ -6,17 +6,5 @@ app = Flask(__name__)
 def home():
     return "Serverul merge!"
 
-if __name__ == '_main_':
-    app.run(debug=True)from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return "Serverul merge!"
-
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-    
