51d1931066435043d6108590cf57380f792d2fab
alexandrachiriac34 <alexandrachiriacc90@gmail.com>
2026-05-28 19:05:04 +0300

    Am rezolvat merge conflict

---
+++ web/app.py
@@ -0,0 +1,22 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Serverul merge!"
+
+if __name__ == '_main_':
+    app.run(debug=True)from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Serverul merge!"
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+    
